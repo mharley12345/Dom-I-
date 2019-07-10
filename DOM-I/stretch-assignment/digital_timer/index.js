fadd2096d59f5f8cfd3360cb4652d694a6d6a4b9
Mike Harley <mharley12345@gmail.com>
2019-07-09 23:17:25 -0700

    'Started Stretch"

---
+++ DOM-I/stretch-assignment/digital_timer/index.js
@@ -0,0 +1,22 @@
+const redDigit = document.querySelectorAll('redDigit')
+
+
+
+// window.addEventListener('mousemove',event =>{
+        
+//        window.addEventListener = event.forEach(console.log(this.event))
+//     // (console.log('move'))
+//     // return events
+// })
+// declares the digital timer variable, declares setInterval function
+// acces the DOM and set text to 00:00
+var digitalTimer = setInterval(function(){
+    document.getElementById('msHundreds').innerHTML = "0";
+    document.getElementById('msTens').innerHTML = "0";
+    document.getElementById('secondOnes').innerHTML = "0";
+    document.getElementById('secondTens').innerHTML = "0";
+
+  
+          
+}    )
+   
