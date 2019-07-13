85bc118c0f31c61b1c5f69ba477edae56092f890
Mike Harley <mharley12345@gmail.com>
2019-07-13 02:18:31 -0700

    Finished Stretch

--- DOM-I/stretch-assignment/digital_timer/index.js
+++ DOM-I/stretch-assignment/digital_timer/index.js
@@ -1,22 +1,103 @@
-const redDigit = document.querySelectorAll('redDigit')
+    
+const digits = document.querySelector(".digits");
+const secondTens = document.getElementById("secondTens");
+const secondOnes = document.getElementById("secondOnes");
+const msHundreds = document.getElementById("msHundreds");
+const msTens = document.getElementById("msTens");
+const msOnes = document.getElementById("msOnes");
+let button = document.createElement('button')
+
+
+timer();
+
+function timer () {
+  init();
+  
+  function init () {
+    let ms = 0;
+    secondTens.innerHTML = "0";
+    secondOnes.innerHTML = "0";
+    msHundreds.innerHTML = "0";
+    msTens.innerHTML = "0";
+    
+    const timerInterval = window.setInterval(() => {
+      ms += 10;
+      if (ms === 10000) {
+        endTimer(timerInterval); 
+      }
+      updateTimer(ms);
+    }, 10);
+  }
+  
+  function increment (string) {
+    let number = Number(string) + 1;
+    return number.toString();
+  }
+
+  function endTimer (intervalId) {
+    clearInterval(intervalId);
+    Array.from(digits.children).forEach(digit => {
+      digit.classList.add("redDigit");
+    })
+  }
+
+  function updateTimer (ms) {
+    if (ms === 10000) {
+      secondTens.innerHTML = "1";
+      secondOnes.innerHTML = "0";
+      msHundreds.innerHTML = "0";
+      msTens.innerHTML = "0";
+    } else if (ms % 1000 === 0) {
+      secondOnes.innerHTML = increment(secondOnes.innerHTML);
+      msHundreds.innerHTML = "0";
+      msTens.innerHTML = "0";
+    } else if (ms % 100 === 0) {
+      msHundreds.innerHTML = increment(msHundreds.innerHTML);
+      msTens.innerHTML = "0";
+    } else {
+      msTens.innerHTML = increment(msTens.innerHTML);
+    }
+  }
+}
+// button.document.className('startBtn')
+// button.appendChild('Start')
+const buttonComponent =()=>{
+
+    return(
+       $`{<Button className='customButton' onClick='timer()'>Start</Button>}`
+    )
+}
+      
+     
+     
 
 
 
-// window.addEventListener('mousemove',event =>{
-        
-//        window.addEventListener = event.forEach(console.log(this.event))
-//     // (console.log('move'))
-//     // return events
-// })
-// declares the digital timer variable, declares setInterval function
-// acces the DOM and set text to 00:00
-var digitalTimer = setInterval(function(){
-    document.getElementById('msHundreds').innerHTML = "0";
-    document.getElementById('msTens').innerHTML = "0";
-    document.getElementById('secondOnes').innerHTML = "0";
-    document.getElementById('secondTens').innerHTML = "0";
+
+
+
+
+
+
+
+
+
+
 
   
+         
+
+
+ 
+    
+
+   
+       
+
+
+
+   
+
           
-}    )
+ 
    
